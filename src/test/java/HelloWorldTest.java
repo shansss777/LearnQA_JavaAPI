bb33ce7d68cc197780288ac300aa5cc76ce60921
Dima <sensus87@inbox.ru>
2023-01-06 01:34:22 +0300

    2lesson, 1 exercise

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -4,7 +4,9 @@ import io.restassured.response.Response;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HelloWorldTest {
@@ -13,12 +15,12 @@ public class HelloWorldTest {
     @Test
     public void testRestAssured() {
 
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "Андрей");
+        //Map<String, String> params = new HashMap<>();
+        //params.put("name", "Андрей");
 
         JsonPath response = RestAssured
                 .given()
-                .queryParams(params)
+                //.queryParams(params)
                 .get("https://playground.learnqa.ru/api/hello")
                 .jsonPath();
         String name = response.get("answer");
@@ -31,11 +33,36 @@ public class HelloWorldTest {
     }
     @Test
     public void testRestAssured2() {
+        Map<String, Object> body = new HashMap<>();
+        body.put("param1","value1");
+        body.put("param2","value2");
+
         Response response = RestAssured
                 .given()
-                .body("param1=value1&param2=value2")
+                .body(body)
                 .post("https://playground.learnqa.ru/api/check_type")
                 .andReturn();
         response.print();
+    }
+    @Test
+    public void testRestStatus() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow( true)
+                .when()
+                .get("https://playground.learnqa.ru/api/get_303")
+                .andReturn();
+        int statusCode = response.getStatusCode();
+        System.out.println(statusCode);
+    }
+    @Test
+    public void testRestHomeWork1() {
+        JsonPath response = RestAssured
+                //.given()
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+        List value = response.get("messages");
+        System.out.println(value.get(0));
     }
 }
