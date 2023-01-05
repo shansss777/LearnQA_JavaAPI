b00cacc7a4737850270a1b7d8d3832a00805541e
Dima <sensus87@inbox.ru>
2023-01-05 19:06:50 +0300

    Anser

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,15 +1,41 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorldTest {
+
+
     @Test
-    public void testHelloWorld(){
+    public void testRestAssured() {
+
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Андрей");
+
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String name = response.get("answer");
+        if (name == null) {
+            System.out.println("Такого ключа 'answer2' не существует");
+        }
+        else {
+            System.out.println(name);
+        }
+    }
+    @Test
+    public void testRestAssured2() {
         Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
-                        .andReturn();
-        String anser = response.getBody().asString();
-        System.out.println(anser);
+                .given()
+                .body("param1=value1&param2=value2")
+                .post("https://playground.learnqa.ru/api/check_type")
+                .andReturn();
+        response.print();
     }
 }
